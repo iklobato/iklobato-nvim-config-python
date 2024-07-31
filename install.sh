69b6707b36c242ed97b09bd720feac48c95418dc
iklobato <iklobato1@gmail.com>
2024-07-31 10:58:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,42 +1,35 @@
 #!/bin/bash
 
-GITHUB_REPO_URL="https://github.com/iklobato/iklobato-nvim-config-python"
-GIT_DIR="$HOME/git"
-CONFIG_DIR="$HOME/.config/nvim"
-BACKUP_SUFFIX=".bak_$(date +%Y%m%d%H%M%S)"
-
-if [ -d "$CONFIG_DIR" ]; then
-    mv "$CONFIG_DIR" "${CONFIG_DIR}${BACKUP_SUFFIX}"
+if [ -d "$HOME/.config/nvim" ]; then
+    mv "$HOME/.config/nvim" "$HOME/.config/nvim.bak_$(date +%Y%m%d%H%M%S)"
 fi
 
-CACHE_DIR="$HOME/.local/share/nvim"
-STATE_DIR="$HOME/.local/state/nvim"
-NVIM_CACHE_DIR="$HOME/.cache/nvim"
-
-if [ -d "$CACHE_DIR" ]; then
-    mv "$CACHE_DIR" "${CACHE_DIR}${BACKUP_SUFFIX}"
+if [ -d "$HOME/.local/share/nvim" ]; then
+    mv "$HOME/.local/share/nvim" "$HOME/.local/share/nvim.bak_$(date +%Y%m%d%H%M%S)"
 fi
 
-if [ -d "$STATE_DIR" ]; then
-    mv "$STATE_DIR" "${STATE_DIR}${BACKUP_SUFFIX}"
+if [ -d "$HOME/.local/state/nvim" ]; then
+    mv "$HOME/.local/state/nvim" "$HOME/.local/state/nvim.bak_$(date +%Y%m%d%H%M%S)"
 fi
 
-if [ -d "$NVIM_CACHE_DIR" ]; then
-    mv "$NVIM_CACHE_DIR" "${NVIM_CACHE_DIR}${BACKUP_SUFFIX}"
+if [ -d "$HOME/.cache/nvim" ]; then
+    mv "$HOME/.cache/nvim" "$HOME/.cache/nvim.bak_$(date +%Y%m%d%H%M%S)"
 fi
 
-mkdir -p "$GIT_DIR"
-cd "$GIT_DIR"
-
-git clone "$GITHUB_REPO_URL" nvim-config
-
-if [ $? -ne 0 ]; then
-    echo "Failed to clone the repository. Exiting."
-    exit 1
+if [ ! -d "$HOME/git" ]; then
+    mkdir "$HOME/git"
 fi
 
-mkdir -p "$CONFIG_DIR"
-cp -r "$GIT_DIR/nvim-config/.config/nvim/"* "$CONFIG_DIR/"
+cd "$HOME/git"
 
-echo "Neovim configuration installed successfully."
+if [ -d "iklobato-nvim-config-python" ]; then
+    rm -rf "iklobato-nvim-config-python"
+fi
 
+git clone https://github.com/iklobato/iklobato-nvim-config-python/
+
+mkdir -p "$HOME/.config/nvim"
+
+cp -r "$HOME/git/iklobato-nvim-config-python/"* "$HOME/.config/nvim/"
+
+echo "Neovim configuration installed successfully. Please open Neovim and run :PlugInstall to install plugins."
