f038747b37a37344b5e4f84239d728341fb95219
iklo <iklobato1+git@gmail.com>
2024-12-16 00:31:32 -0300

    change export/import script

--- mac_packs.sh
+++ mac_packs.sh
@@ -3,42 +3,38 @@
 output_file="installed_packages.txt"
 
 export_packages() {
-    echo "Homebrew packages:" > $output_file
-    brew leaves | xargs brew desc --eval-all >> $output_file
-
+    echo "Homebrew packages:" > "$output_file"
+    brew leaves | xargs brew desc --eval-all >> "$output_file"
     echo "Package list saved to $output_file"
 }
 
-install_brew_packages() {
-    while read -r line; do
-        if [[ ! -z "$line" && "$line" != "Homebrew packages:" ]]; then
-            package=$(echo "$line" | cut -d: -f1)
-            brew install "$package"
+import_packages() {
+    if [[ ! -f "$output_file" ]]; then
+        echo "Error: $output_file not found."
+        exit 1
+    fi
+
+    sed '1d' "$output_file" | while read -r line; do
+        if [[ ! -z "$line" ]]; then
+            package=$(echo "$line" | awk '{print $1}')
+            if [[ ! -z "$package" ]]; then
+                echo "Installing $package..."
+                brew install "$package"
+            fi
         fi
     done
 }
 
-import_packages() {
-    while IFS= read -r line; do
-        case "$line" in
-            "Homebrew packages:")
-                install_brew_packages
-                ;;
-        esac
-    done < "$output_file"
-}
-
-if [[ "$1" == "export" ]]; then
-    export_packages
-elif [[ "$1" == "import" ]]; then
-    if [[ -f "$output_file" ]]; then
+case "$1" in
+    "export")
+        export_packages
+        ;;
+    "import")
         import_packages
-    else
-        echo "Error: $output_file not found."
+        ;;
+    *)
+        echo "Usage: $0 {export|import}"
         exit 1
-    fi
-else
-    echo "Usage: $0 {export|import}"
-    exit 1
-fi
+        ;;
+esac
 
