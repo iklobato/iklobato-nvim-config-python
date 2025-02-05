fdbe853258cce3115ac167fb391ae0a594f793d0
iklobato <iklobato1+git@gmail.com>
2025-02-05 12:50:52 -0300

    feat(plugins): configure nvim-surround

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,9 +1,12 @@
 -- Add, Change, Delete Surrounding Chars (["''"])
 return {
-  -- https://github.com/kylechui/nvim-surround
-  'kylechui/nvim-surround',
-  version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  event = "VeryLazy",
-  opts = {}
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+
+        })
+    end
 }
 
