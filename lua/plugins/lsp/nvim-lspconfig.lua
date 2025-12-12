0ef0c9fd697275a5776eea514a9857ae2d80a6dd
iklobato <iklobato1@gmail.com>
2025-12-12 16:49:35 -0300

    refactor(plugins): streamline LSP and Copilot configurations - Removed redundant completion setup from nvim-lspconfig.lua, delegating to nvim-cmp...

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -50,59 +50,8 @@ return {
             -- Servers will be automatically installed when configured in lua/lsp/
         })
 
-        -- Setup completion
-        local cmp = require("cmp")
-
-        local has_words_before = function()
-            unpack = unpack or table.unpack
-            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-        end
-
-        cmp.setup({
-            window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ 
-                    select = true,
-                    behavior = cmp.ConfirmBehavior.Replace 
-                }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp', priority = 1000 }, -- LSP - highest priority
-                { name = 'buffer', priority = 500, max_item_count = 5 }, -- Buffer - limited for performance
-                { name = 'path', priority = 250 }, -- Path
-            }),
-            performance = {
-                debounce = 60, -- Debounce completion requests (ms)
-                throttle = 30, -- Throttle completion requests (ms)
-                fetching_timeout = 500, -- Timeout for fetching completions (ms)
-            },
-            formatting = {
-                format = function(entry, vim_item)
-                    vim_item.kind = string.format('%s', vim_item.kind)
-                    vim_item.menu = ({
-                        nvim_lsp = "[LSP]",
-                        buffer = "[Buffer]",
-                        path = "[Path]",
-                    })[entry.source.name]
-                    return vim_item
-                end
-            },
-        })
+        -- Note: cmp.setup() is configured in lua/plugins/completion/nvim-cmp.lua
+        -- This avoids duplicate configuration and conflicts
 
         -- Setup buffer-local mappings and options
         local on_attach = function(client, bufnr)
