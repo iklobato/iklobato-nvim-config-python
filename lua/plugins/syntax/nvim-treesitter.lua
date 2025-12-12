5c710ff63efb5e11b906cb653c385eb2d0adc95a
iklobato <iklobato1@gmail.com>
2025-12-12 01:40:24 -0300

    perf(plugins): optimize keymaps and Treesitter Optimize keymaps and Treesitter for better performance: Keymaps optimizations: - Cache...

--- lua/plugins/syntax/nvim-treesitter.lua
+++ lua/plugins/syntax/nvim-treesitter.lua
@@ -11,22 +11,71 @@ return {
   opts = {
     indent = { enable = true },
     highlight = { enable = true },
-    auto_install = true, -- automatically install syntax support when entering new file type buffer
+    auto_install = false, -- Disabled for performance - install parsers manually with :TSInstall
     ensure_installed = {
       'lua',
+      'dockerfile',
       'javascript',
+      'typescript',
+      'tsx',
       'html',
+      'css',
+      'scss',
       'python',
       'markdown',
       'json',
       'yaml',
       'bash',
+      'vim',
+      'xml',
+      'csv',
       'hcl'
+    },
+    -- Textobjects configuration - lazy loaded for performance
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          -- Only enable essential textobjects to reduce overhead
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+      },
     },
   },
   config = function (_, opts)
     local configs = require("nvim-treesitter.configs")
     configs.setup(opts)
+    vim.api.nvim_create_autocmd({ "FileType" }, {
+      pattern = { "typescript", "typescriptreact", "typescript.tsx" },
+      callback = function()
+        -- Disable Vim syntax to let Treesitter handle it completely
+        vim.wo.syntax = "OFF"
+      end,
+    })
   end
 }
 
