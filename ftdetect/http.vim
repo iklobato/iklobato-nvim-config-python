c41ca430d5f777a04c1f7652f9d303bd8595449f
iklobato <iklobato1@gmail.com>
2026-02-02 16:23:45 -0300

    chore: remove legacy runtime files
    Drop unused runtime artifacts now that the config no longer references them.

--- ftdetect/http.vim
+++
@@ -1,3 +0,0 @@
-" Filetype detection for HTTP/REST files
-au BufNewFile,BufRead *.http setfiletype http
-au BufNewFile,BufRead *.rest setfiletype http
--- ftplugin/rest.lua
+++
@@ -1,7 +0,0 @@
--- REST file specific settings (same as HTTP)
-vim.opt_local.syntax = "on"
-vim.opt_local.filetype = "http"
-
--- Load HTTP syntax highlighting
-vim.cmd('runtime! syntax/http.vim')
-vim.cmd('syntax enable')
