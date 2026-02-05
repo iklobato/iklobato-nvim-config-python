00b1946b266d5630661867afeb00e1efe666313c
iklobato <iklobato1@gmail.com>
2026-02-04 23:10:03 -0300

    feat: return <leader>fo feature with better resizing

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -15,6 +15,12 @@ end, { desc = "Live grep" })
 map("n", "<leader>fb", function()
   require("telescope.builtin").buffers()
 end, { desc = "Buffers" })
+map("n", "<leader>fo", function()
+  local w = vim.api.nvim_win_get_width(0)
+  require("telescope.builtin").lsp_document_symbols({
+    symbol_width = math.max(40, math.floor(w * 0.5)),
+  })
+end, { desc = "Find symbols" })
 
 -- Diagnostics
 map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Diagnostic float" })
