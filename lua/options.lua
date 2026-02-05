d8fe1e70f348d07d3a034f88b4ca9d99f2a16e10
iklobato <iklobato1@gmail.com>
2026-02-04 22:58:24 -0300

    feat: add filename as tab anem

--- lua/options.lua
+++ lua/options.lua
@@ -17,3 +17,19 @@ opt.clipboard:append("unnamedplus")
 
 opt.mouse = ""
 opt.swapfile = false
+
+-- Tabline: show filename (tail) per tab
+local function tabline_build()
+  local cur, parts = vim.api.nvim_get_current_tabpage(), {}
+  for i, tab in ipairs(vim.api.nvim_list_tabpages()) do
+    local buf = vim.api.nvim_win_get_buf(vim.api.nvim_tabpage_get_win(tab))
+    local path = vim.api.nvim_buf_get_name(buf)
+    local name = (path == "" or path == nil) and "[No Name]" or vim.fn.fnamemodify(path, ":t")
+    name = (string.gsub(name, "%%", "%%%%"))
+    parts[#parts + 1] = ("%s%%%dT %s %%X"):format(tab == cur and "%#TabLineSel#" or "%#TabLine#", i, name)
+  end
+  parts[#parts + 1] = "%#TabLineFill#%T"
+  return table.concat(parts, "")
+end
+_G.tabline_build = tabline_build
+vim.opt.tabline = "%!v:lua.tabline_build()"
